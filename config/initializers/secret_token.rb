ac26f0dc6abba9854ccf8b0163a37bd71f160184
Mrinal Agarwal <mrinal@infotechoutreach.org>
2013-03-13 00:08:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OttographApp::Application.config.secret_token = 'c08d8e6d37b0e66102c77d48dfb59219660daadd3f31647b61148a16125e41360ba43446d4d87fa100675dfbc30002c12f51b530a25c73aa6b2c809ee34d0e7f'
